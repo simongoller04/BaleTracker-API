473afabbf83ea36628d3c90b663c10b6373accd3
Simon Goller <simon.goller4@gmail.com>
2024-03-24 13:20:15 +0100

    update: dockerfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	war
 	id("org.springframework.boot") version "3.1.5"
 	id("io.spring.dependency-management") version "1.1.3"
 	kotlin("jvm") version "1.8.22"
